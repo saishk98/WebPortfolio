818e6ea1af4069fef6e663878222ac0013ec3f6c
saishk98 <89117911+saishk98@users.noreply.github.com>
2021-11-17 16:51:33 +0800

    Add files via upload

---
+++ Web Portfolio/index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Saish's Web Portfolio</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" href="images/icon.png">
+	<link href="assets/css/docs.css?v=029b1bc45e0ff9727b1fb1bd03f8d4f0" rel="stylesheet" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+	<section id="main">
+		<!--<nav>
+			<ul class="menu">
+				<li><a href="#main">Home</a></li>
+				<li><a href="#skills">About</a></li>
+				<li><a href="#portfolio-work">Portfolio</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</nav>-->
+		<ul class="menu menu-hover-lines">
+			<li><a href="#main">Home</a></li>
+			<li><a href="#skills">About</a></li>
+			<li><a href="#portfolio-work">Portfolio</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>		  
+		<div class="content">
+			<div class="model">
+				<img src="images/saish-logo-1.svg" alt="saish-logo">
+			</div>
+			<div class="main-text">
+				<h1>Hello, I'm Saish Kothare</h1>
+				<p>I am a Full-Stack Web-Developer. I work on both FrontEnd (HTML, CSS, JavaScript, SEO...) and BackEnd (PHP, SQL, GIT...) languages and frameworks, as well as in server, network and hosting environments.</p>
+				<a href="saish_resume.pdf" class="resume-btn">Download Resume</a>
+			</div>
+		</div>
+	</section>
+	<section id="skills">
+		<div class="skills-content">
+			<div class="more-aboutme">
+				<h1>About</h1>
+				<img src="images/me.png" alt="me" class="me" style="float: left;">
+				<p>As a Full-Stack Web-Developer, my primary responsibility includes designing user interactions on websites, developing servers and databases for website functionality and coding for mobile platforms.</p>
+			</div>
+			<div class="fs-dev">
+				<img src="images/fs-dev.jpg" alt="fs-dev">
+			</div>
+		</div>
+	</section>
+	<section id="portfolio-work">
+		<div class="portfolio">
+			<h1>Portfolio</h1>
+			<h2>Certificate IV Web-Based Technologies</h2>
+			<div class="row small-up-1 medium-up-2 large-up-3">
+				<div class="column">
+				  <div class="callout">
+					<p><a href="accessibility_report.pdf"><img src="images/accessibility-testing.jpg" alt="accessibility-testing"></a></p>
+					<p class="lead"><a href="accessibility_report.pdf">Accessbility & Testing</a></p>
+					<p class="subheader">Accessibility testing is the practice of making your web and mobile apps usable to as many people as possible. It makes apps accessible to those with disabilities, such as vision impairment, hearing disabilities, and other physical or cognitive conditions.</p>
+				  </div>
+				</div>
+				<div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/backend-development.jpg" alt="backend-development"></a></p>
+					  <p class="lead"><a href="#">BackEnd Development: Scripting</a></p>
+					  <p class="subheader">Back end development refers to the server side of an application and everything that communicates between the database and the browser.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="database.pdf"><img src="images/create-basic-databases.jpg" alt="create-basic-databases"></a></p>
+					  <p class="lead"><a href="database.pdf">Create Basic Databases</a></p>
+					  <p class="subheader">A database is an organised collection of structured information or data stored electronically in a computer system. A database is usually controlled by a database management system (DBMS). Together, the data and the DBMS, along with the applications that are associated with them, are referred to as a database system, often shortened to just database.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/digital-layout-design.jpg" alt="digital-layout-design"></a></p>
+					  <p class="lead"><a href="#">Design Layout Design</a></p>
+					  <p class="subheader">Digital design is a type of visual communication that presents information or a product or service through a digital interface.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/frontend-development-html.jpg" alt="frontend-development-html"></a></p>
+					  <p class="lead"><a href="#">FrontEnd Development: HTML</a></p>
+					  <p class="subheader">The front end of a website is everything the user either sees or interacts with when they visit the website. It is responsible for the total look and feel of an online experience.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/frontend-development-js.jpg" alt="accessibility-testing"></a></p>
+					  <p class="lead"><a href="#">FrontEnd Development: JavaScript</a></p>
+					  <p class="subheader">JavaScript is a very powerful tool that can do many things for a website. For one, it powers the site's general interactivity. JavaScript makes it possible to build rich UI components such as image sliders, pop-ups, site navigation mega menus, form validations, tabs, accordions, and much more.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/ux-design.jpg" alt="ux-design"></a></p>
+					  <p class="lead"><a href="ux-design-layout.pdf">UX Design</a></p>
+					  <p class="subheader">User experience (UX) design is the process design teams use to create products that provide meaningful and relevant experiences to users. This involves the design of the entire process of acquiring and integrating the product, including aspects of branding, design, usability and function.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/web-security.jpg" alt="web-security"></a></p>
+					  <p class="lead"><a href="#">Web Security</a></p>
+					  <p class="subheader">Web Security is the protective measures and protocols that organizations adopt to protect the organization from, cyber criminals and threats that use the web channel. Web security is critical to business continuity and to protecting data, users and companies from risk.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/findability.jpg" alt="accessibility-testing"></a></p>
+					  <p class="lead"><a href="seo_report.pdf">Website Findability</a></p>
+					  <p class="subheader">Findability is the ease with which information can be found. It means that users can easily find content or information they assume is present on a website. </p>
+					</div>
+				  </div>
+			  </div><br>
+			  <h2>Diploma of Website Development</h2>
+			<div class="row small-up-1 medium-up-2 large-up-3">
+				<div class="column">
+				  <div class="callout">
+					<p><a href="#"><img src="images/oop.jpg" alt="oop"></a></p>
+					<p class="lead"><a href="#">Apply skills in object-oriented design</a></p>
+					<p class="subheader">Object-oriented design (OOD) is the process of using an object-oriented methodology to design a computing system or application. This technique enables the implementation of a software solution based on the concepts of objects. OOD serves as part of the object-oriented programming (OOP) process or lifecycle.</p>
+				  </div>
+				</div>
+				<div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/client-testing.jpg" alt="client-testing"></a></p>
+					  <p class="lead"><a href="#">Client Testing</a></p>
+					  <p class="subheader">In client-side testing, once a user requests a page, your server delivers it. BUT, in this case, your experimentation tool implements some Javascript inside your user’s browser to alter the content delivered by the server so that the end user gets the appropriate variation based on your targeting rules. (The browser is the client.)</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/complex-ux-design.jpg" alt="complex-ux-design"></a></p>
+					  <p class="lead"><a href="#">Complex UX Design</a></p>
+					  <p class="subheader">Complex UX Design is the next level in advance from the set of skills gained and acquired from the UX Design Cluster in Certificate IV.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/cms.jpg" alt="cms"></a></p>
+					  <p class="lead"><a href="#">Customise CMS</a></p>
+					  <p class="subheader">A content management system (CMS) is an application that is used to manage web content, allowing multiple contributors to create, edit and publish. Content in a CMS is typically stored in a database and displayed in a presentation layer based on a set of templates.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/develop-webinfo-architecture.jpg" alt="develop-webinfo-architecture"></a></p>
+					  <p class="lead"><a href="#">Develop Website Information Architecture</a></p>
+					  <p class="subheader">Information architecture is the process of identifying and defining those particular needs and creating a structure and nomenclature for them.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/project-management.jpg" alt="project-management"></a></p>
+					  <p class="lead"><a href="#">Project Management</a></p>
+					  <p class="subheader">Project management is the application of processes, methods, skills, knowledge and experience to achieve specific project objectives according to the project acceptance criteria within agreed parameters. Project management has final deliverables that are constrained to a finite timescale and budget.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/web-application-development.jpg" alt="web-application-development"></a></p>
+					  <p class="lead"><a href="#">Web Application Development</a></p>
+					  <p class="subheader">Web application development is the creation of application programs that reside on remote servers and are delivered to the user's device over the Internet. A web application (web app) does not need to be downloaded and is instead accessed through a network.</p>
+					</div>
+				  </div>
+				  <div class="column">
+					<div class="callout">
+					  <p><a href="#"><img src="images/Brickin-It.png" alt="Brickin-It"></a></p>
+					  <p class="lead"><a href="#">Client Project: Lego</a></p>
+					  <p class="subheader">This project was a group project where we are assigned to work with a client who wants to fulfill his requirements for his website.</p>
+					</div>
+				  </div>
+			  </div>
+		</div>
+	</section>
+	<section id="contact">
+		<h1 class="contactme-h1">Contact Me</h1><br>
+		<div class="contact-form">
+			<div class="row">
+				<div class="medium-6 columns">
+					<h2 class="contactme-h2">Get In Touch</h2><br>
+					<div class="responsive-embed">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3385.474779062766!2d115.85886871511634!3d-31.94801408123027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32bacdf53147a1%3A0x6a13f7dd60dab2c8!2sNorth%20Metropolitan%20TAFE!5e0!3m2!1sen!2sau!4v1634545433219!5m2!1sen!2sau" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" alt="Google Map of NM TAFE"></iframe>
+					</div>
+					<ul class="contact-us-list">
+						<li class="address">25 Aberdeen Street, Perth, 6003</li>
+						<li class="email"><a href="mailto:">J269072@tafe.wa.edu.au</a></li>
+						<li class="phone">
+							1300 300 822
+						</li>
+					</ul>
+				</div>
+				<div class="medium-6 columns">
+					<h2 class="contactme-h2">Mail Us</h2><br>
+				  <label>First Name
+					<input type="text" placeholder="First Name">
+				  </label>
+				  <label>Last Name
+					<input type="text" placeholder="Last Name">
+				  </label>
+				  <label>Email
+					<input type="text" placeholder="Email">
+				  </label>
+				  <label>Subject
+					<input type="text" placeholder="Subject">
+				  </label>
+				  <label>
+					Message
+					<textarea placeholder="Type a Message"></textarea>
+				  </label>
+				  <input type="submit" class="button expanded" value="Submit" style="background-color: deepskyblue;">
+				</div>
+			  </div>
+		</div>
+	</section> 
+</body>
+</html>
