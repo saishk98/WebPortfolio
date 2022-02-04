210c14de38556b3d02cc0266e6d5e9f675c83fef
saishk98 <89117911+saishk98@users.noreply.github.com>
2022-02-04 19:04:53 +0800

    Delete Web Portfolio directory

--- Web Portfolio/js/script.js
+++
@@ -1,7 +0,0 @@
-/* Typing Animation */
-var typed = new Typed(".typing",{
-    strings:["","Web Designer", "Web Developer", "UX/UI Designer", "Full Stack Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
