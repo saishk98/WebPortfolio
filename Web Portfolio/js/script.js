b84ce706db04c6fb0125bd0c2dcfd04dba6fad4a
saishk98 <89117911+saishk98@users.noreply.github.com>
2022-02-04 19:03:09 +0800

    Add files via upload

---
+++ Web Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web Developer", "UX/UI Designer", "Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
